513b4459b97d8b7b8cac24696a1452cfdc45ab22
Wanchen-W <wanchenwork@gmail,com>
2024-04-09 20:23:29 -0400

    finished unit testings

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,46 +1,41 @@
-// import necessary react testing library helpers here
-// import the Counter component here
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import Counter from "../components/Counter.js";
+import Counter from '../components/Counter.js'; // Adjust the path accordingly
+
+let component;
 
 beforeEach(() => {
-    <Counter />;
-  // Render the Counter component here
-})
+    component = render(<Counter />);
+});
 
 test('renders counter message', () => {
-    const { getByText } = render(<Counter />);
-    const counterMessage = getByText("Counter");
+    const { getByText } = component;
+    const counterMessage = getByText('Counter');
     expect(counterMessage).toBeInTheDocument();
-  // Complete the unit test below based on the objective in the line above
 });
 
 test('should render initial count with value of 0', () => {
-    const { getByText } = render(<Counter />);
-   // const countValue = getByText("count");
-    expect(getByText("count")).toHaveTextContent("0");
-  // Complete the unit test below based on the objective in the line above
+    const { getByTestId } = component;
+    const countValue = getByTestId('count'); // Look for element with data-testid="count"
+    expect(countValue.textContent).toBe('0');
 });
 
 test('clicking + increments the count', () => {
-    const { getByTestId, getByText } = render(<Counter />);
-   // const countValue = getByTestId("count");
-   // const incrementButton = getByText("+");
+    const { getByTestId, getByText } = component;
+    const countValue = getByTestId('count');
+    const incrementButton = getByText('+');
 
-    fireEvent.click(getByText("+"));
+    fireEvent.click(incrementButton);
 
-    expect(getByTestId("count")).toHaveTextContent("1");
-  // Complete the unit test below based on the objective in the line above
+    expect(countValue.textContent).toBe('1');
 });
 
 test('clicking - decrements the count', () => {
-    const { getByTestId, getByText } = render(<Counter />);
-  //  const countValue = getByTestId("count");
-  //  const decrementButton = getByText("-");
+    const { getByTestId, getByText } = component;
+    const countValue = getByTestId('count');
+    const decrementButton = getByText('-');
 
-    fireEvent.click(getByText("-"));
+    fireEvent.click(decrementButton);
 
-    expect(getByTestId("count")).toHaveTextContent("-1");
-  // Complete the unit test below based on the objective in the line above
+    expect(countValue.textContent).toBe('-1');
 });
